4a5deee01a4af624e6d6d5f27c1d7d04fb66839a
Morten Gislund <mortengislund@gmail.com>
2024-05-22 21:00:50 +0200

    check

--- view/view.js
+++ view/view.js
@@ -27,25 +27,25 @@ export class GridSearchView {
             this.boardElement.appendChild(rowElement);
         }
         this.boardElement.style.gridTemplateColumns=`repeat(${WIDTH}, 4px) `;
-        // this.boardElement.computedStyleMap.gridTemplateRowss=`repeat(${HEIGHT}, 4px) `;
+         this.boardElement.style.gridTemplateRows=`repeat(${HEIGHT}, 4px) `;
     }
     // ### skal omskrives så den opdaterer celler/nodes tilføjes queue eller er del af path ###
 
-    // updateView() {
-    //     const cells = this.boardElement.querySelectorAll('.cell');
-    //     cells.forEach(cell => {
-    //         const row = parseInt(cell.dataset.row);
-    //         const col = parseInt(cell.dataset.col);
-    //         const cellState = this.model.getCell(row, col);
-    //         if (cellState) {
-    //             cell.classList.add('alive');
-    //         } else {
-    //             cell.classList.remove('alive');
-    //         }
-    //     });
-    //     const generations=document.querySelector("#generations");
-    //     generations.value=`Generations: ${this.model.generations}`;
-    // }
+    updateView() {
+        const cells = this.boardElement.querySelectorAll('.cell');
+        cells.forEach(cell => {
+            const row = parseInt(cell.dataset.row);
+            const col = parseInt(cell.dataset.col);
+            const cellState = this.model.getCell(row, col);
+            if (cellState) {
+                cell.classList.add('alive');
+            } else {
+                cell.classList.remove('alive');
+            }
+        });
+        
+        generations.value=`Generations: ${this.model.generations}`;
+    }
     makeBoardClickable(){
         this.boardElement.addEventListener("click", this.boardClicked.bind(this));
     }
